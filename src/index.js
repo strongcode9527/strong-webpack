ab47448e1e277a3714a74cbeab7d6ec53c6f4222
strongcode9527 <1477869317@qq.com>
2018-01-16 18:18:08 +0800

    :sparkles: 研究esprima包的解析结果，并将所有对象的所有节点全部打印出来

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ require('babel-register')
 var path = require('path'),
     fs = require('fs'),
     config = require('../webpack/index.js'),
-    esprima = require('esprima')
-
+    esprima = require('esprima'),
+    inspect = require('util').inspect
 
 fs.readFile(config.entry, 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(esprima.tokenize(data));
+  console.log(inspect(esprima.parse(data).body[0], { showHidden: true, depth: null }));
 });
 
-console.log(config)
+// : 定义变量
+// type: VariableDeclarator
+// => init
+// type: CallExpression: 函数调用
+//
